68911fa6060cd8eb4433fbc22808e69505fcc74d
Jeremy Lardenois <jeremy.lardenois@etu.umontpellier.fr>
2022-11-23 16:53:39 +0100

    feat: create a downloadable zip for json

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -1,5 +1,12 @@
 import MaterialButton from "react-materialize/lib/Button";
 import { useState } from "react";
+import pullRequests from "data/gh-pull-request.json"
+import issueEvent from "data/gh-issue-event.json"
+import pushEvent from "data/gh-push-event.json"
+import licenses from "data/github-licenses.json"
+import starEvent from "data/gh-star-event.json"
+import { saveAs } from 'file-saver';
+import JSZip from 'jszip'
 
 export default function DownloadButton({ _match, _store, _history }) {
     const className = "waves-effect waves-light blue-grey darken-1";
@@ -23,6 +30,28 @@ export default function DownloadButton({ _match, _store, _history }) {
         borderBottomLeftRadius: "0",
         borderTopLeftRadius: "0",
     }
+    
+    const jsonFiles = {
+        "gh-issue-event.json": issueEvent,
+        "gh-push-event.json": pushEvent,
+        "github-licenses.json": licenses,
+        "gh-pull-request.json": pullRequests,
+        "gh-star-event.json": starEvent
+    }
+
+    const downloadJSONZip = () => {
+        const zipJson = new JSZip();
+        for (const [filename, file] of Object.entries(jsonFiles)) {
+            zipJson.file(filename, [file])
+        }
+        // download("githut-json.zip", zipJson)
+        zipJson.generateAsync({type:"blob"})
+            .then(function(content) {
+                // see FileSaver.js
+                saveAs(content, "githut-json.zip");
+            });
+
+    }
 
     return (
         <div>
@@ -34,8 +63,8 @@ export default function DownloadButton({ _match, _store, _history }) {
                         </center>
                     ) : (
                         <center>
-                            <MaterialButton className={className} style={styleLinkedButtonLeft} download="./data/data-csv.zip"> In CSV </MaterialButton>
-                            <MaterialButton className={className} style={styleLinkedButtonRight} download="./data/data-csv.zip"> In JSON </MaterialButton>
+                            <MaterialButton className={className} style={styleLinkedButtonLeft} onClick={downloadJSONZip}> In JSON </MaterialButton>
+                            <MaterialButton className={className} style={styleLinkedButtonRight}> In CSV </MaterialButton>
                         </center>
                     )}
                 </div>
